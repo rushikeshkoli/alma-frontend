e8e57e07b8b78120fc5ef783413db56601318a48
rushi <rushikeshkoli3@gmail.com>
2021-05-25 02:06:58 +0530

    added the readme

--- src/Components/Leaderboard.tsx
+++ src/Components/Leaderboard.tsx
@@ -1,22 +1,25 @@
-import { Container, TableBody, TableCell, TableContainer, TableHead, TableRow, Table } from "@material-ui/core";
-import React, { useEffect, useState } from "react";
+import { Container, TableBody, TableCell, Button, TableContainer, TableHead, TableRow, Table, TableSortLabel, ButtonBase, Box, Icon, Input, OutlinedInput } from "@material-ui/core";
+import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
+import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward';
+import React, { ChangeEvent, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
+
 interface HeadCell {
-  id: number,
+  id: string,
   disablePadding: boolean;
   label: string;
   numeric: boolean;
 }
 
 const headCells: HeadCell[] = [
-  { id: 1, numeric: false, disablePadding: true, label: "Name" },
-  { id: 2, numeric: false, disablePadding: true, label: "Maths" },
-  { id: 3, numeric: false, disablePadding: true, label: "Physics" },
-  { id: 4, numeric: false, disablePadding: true, label: "Chemistry" },
-  { id: 5, numeric: true, disablePadding: false, label: "Total" },
-  { id: 6, numeric: true, disablePadding: false, label: "Percentage" }
+  { id: 'name', numeric: false, disablePadding: true, label: "Name" },
+  { id: 'maths', numeric: false, disablePadding: true, label: "Maths" },
+  { id: 'physics', numeric: false, disablePadding: true, label: "Physics" },
+  { id: 'chemistry', numeric: false, disablePadding: true, label: "Chemistry" },
+  { id: 'total', numeric: true, disablePadding: true, label: "Total" },
+  { id: 'percentage', numeric: true, disablePadding: true, label: "Percentage" }
 ];
 
 const data = [
@@ -42,15 +45,114 @@ const data = [
   }
 ];
 
+function descendingComparator(a: any, b: any, orderBy: any) {
+  console.log(a[orderBy])
+  console.log(b[orderBy])
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function stableSort(array: any, order: any, orderBy: any) {
+  const stabilizedThis = array.map((el: any, index: any) => [el, index]);
+  stabilizedThis.sort((a: any, b: any) => {
+    console.log(a[0])
+    let temp
+    if (order === 'desc') {
+      temp = descendingComparator(a[0], b[0], orderBy)
+    } else {
+      temp = -descendingComparator(a[0], b[0], orderBy)
+    }
+    if (temp !== 0) return temp;
+    return a[1] - b[1];
+  });
+  console.log(stabilizedThis)
+  return stabilizedThis.map((el: any) => el[0]);
+}
+
+// {stableSort(rows, getComparator(order, orderBy))
+//   .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+//   .map((row, index) => {
+//     const isItemSelected = isSelected(row.name);
+//     const labelId = `enhanced-table-checkbox-${index}`;
+
+//     return (
+//       <TableRow
+//         key={row.name}
+//       >
+//         <TableCell
+//           component="th"
+//           id={labelId}
+//           scope="row"
+//           padding="none"
+//         >
+//           {row.name}
+//         </TableCell>
+//         <TableCell align="right">{row.calories}</TableCell>
+//         <TableCell align="right">{row.fat}</TableCell>
+//         <TableCell align="right">{row.carbs}</TableCell>
+//         <TableCell align="right">{row.protein}</TableCell>
+//       </TableRow>
+//     );
+//   })}
+
 const Leaderboard = () => {
   const [students, setStudents] = useState<any>([])
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState<string>('percentage');
+  const [searchVal, setSearchVal] = useState<string>('')
+
+  const handleSearchButtonClick = () => {
+    const data: any = []
+    localStorage.setItem('students', JSON.stringify(students))
+    students.map((student: any) => {
+      if (student.name.search(searchVal) !== -1) {
+        console.log(student)
+        data.push(student)
+      }
+    })
+    console.log(data)
+    setStudents(data)
+  }
+
+  const handleSearchValChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchVal(e.target.value)
+    if (!e.target.value) {
+      // fetchdata()
+      const temp = localStorage.getItem('students')
+      if (temp) {
+        setStudents(JSON.parse(temp))
+        console.log(JSON.parse(temp))
+      }
+    }
+  }
+
+  async function fetchdata() {
+    const res = await axios.get(`${process.env.REMOTE_HOST}/api/leaderboard/`)
+    console.log(res.data.scoreboard)
+    setStudents(res.data.scoreboard)
+  }
+
+  const handleSortClick = (sortBy: any) => {
+    if (orderBy === sortBy) {
+      setOrder(order === 'asc' ? 'desc' : 'asc')
+    } else {
+      setOrder('asc')
+      setOrderBy(sortBy)
+    }
+  }
 
   useEffect(() => {
-    async function fetchdata() {
-      const res = await axios.get('http://localhost:8000/api/leaderboard/')
-      console.log(res)
-      setStudents(res.data.scoreboard)
-    }
+    const data = stableSort(students, order, orderBy)
+    console.log(data)
+    setStudents(data)
+  }, [order, orderBy])
+
+  useEffect(() => {
     fetchdata()
   }, [])
 
@@ -58,6 +160,11 @@ const Leaderboard = () => {
     <div>
       <Container>
         <Link to="/">Home</Link>
+        <div>
+          <OutlinedInput placeholder="Search" onChange={handleSearchValChange} />
+          <Button style={{ marginLeft: '1rem' }} color='primary' variant="outlined" onClick={handleSearchButtonClick}>Search</Button>
+          {/* <OutlinedInput id="outlined-basic" placeholder="Search Branch" onChange={handleBranchChange} value={branchName} /> */}
+        </div>
         <TableContainer>
           <Table>
             <TableHead>
@@ -66,30 +173,34 @@ const Leaderboard = () => {
                   <TableCell
                     key={headCell.id}
                   >
-                    {headCell.label}
+                    <ButtonBase style={{ width: '100%', borderRight: '2px solid #eee' }} onClick={() => handleSortClick(headCell.id)}>
+                      {headCell.label}
+                      {orderBy === headCell.id && (order === 'asc' ? <ArrowUpwardIcon fontSize='small' /> : <ArrowDownwardIcon fontSize='small' />)}
+
+                    </ButtonBase>
                   </TableCell>
                 ))}
               </TableRow>
             </TableHead>
             <TableBody>
               {students.map((student: any) => (
-                <TableRow>
+                <TableRow key={student.roll_no}>
                   <TableCell>
                     {student.name}
                   </TableCell>
-                  <TableCell>
+                  <TableCell align='right'>
                     {student.maths}
                   </TableCell>
-                  <TableCell>
+                  <TableCell align='right'>
                     {student.physics}
                   </TableCell>
-                  <TableCell>
+                  <TableCell align='right'>
                     {student.chemistry}
                   </TableCell>
-                  <TableCell>
+                  <TableCell align='right'>
                     {student.total}
                   </TableCell>
-                  <TableCell>
+                  <TableCell align='right'>
                     {student.percentage}
                   </TableCell>
                 </TableRow>
--- src/Components/NewRecord.tsx
+++ src/Components/NewRecord.tsx
@@ -1,6 +1,8 @@
 import React, { FormEvent, useState, ChangeEvent } from 'react';
 import { Container, Input, TextField, Button } from '@material-ui/core';
-import {Link} from 'react-router-dom'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+import { Link } from 'react-router-dom'
 import axios from "axios";
 // - Following inputs should be taken from the frontend:
 // - Roll No
@@ -12,7 +14,20 @@ import axios from "axios";
 // - Percentage (Automatically calculated)
 // - Extra points for basic validation on the input fields
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& .MuiTextField-root': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+  }),
+);
+
 const NewRecord = () => {
+  const classes = useStyles();
+
   const [rollNo, setRollNo] = useState<number>();
   const [name, setName] = useState<string>();
   const [total, setTotal] = useState<number>();
@@ -20,12 +35,13 @@ const NewRecord = () => {
   const [mathsScore, setMathsScore] = useState('' as string);
   const [physicsScore, setPhysicsScore] = useState('' as string);
   const [chemScore, setChemScore] = useState('' as string);
+  const [errorField, setErrorFeild] = useState<string>('')
 
 
   const sendData = (e: FormEvent) => {
     e.preventDefault();
     console.log("submitted");
-    axios.post('http://localhost:8000/api/result/', {
+    axios.post(`${process.env.REMOTE_HOST}/api/result/`, {
       'roll_no': rollNo,
       'name': name,
       'maths': mathsScore,
@@ -43,6 +59,15 @@ const NewRecord = () => {
     // if (!score) {
     //   return
     // }
+    const int_score = parseInt(score)
+    if (int_score < 0 || int_score > 100) {
+      setErrorFeild(sub)
+    } else {
+      if (errorField === sub) {
+        setErrorFeild('')
+      }
+    }
+    // if (score)
     if (sub === 'maths') {
       setMathsScore(score)
     } else if (sub === 'physics') {
@@ -67,19 +92,19 @@ const NewRecord = () => {
   return (
     <div>
       <Container>
-      <Link to="/">Home</Link>
-        <form onSubmit={sendData}>
-          <TextField label="Roll Number" onChange={handleRollNoChange}/>
-          <TextField label="Name" onChange={handleNameChange}/>
-          <br />
-          <h3>Scores</h3>
-          <TextField label="Maths" value={mathsScore} onChange={(val) => handleScoreChange(val, "maths")}/>
-          <TextField label="Physics" value={physicsScore} onChange={(val) => handleScoreChange(val, "physics")}/>
-          <TextField label="Chemistry" value={chemScore} onChange={(val) => handleScoreChange(val, "chem")}/>
+        <Link to="/">Home</Link>
+        <form className={classes.root} onSubmit={sendData}>
+          <TextField label="Roll Number" onChange={handleRollNoChange} />
+          <TextField label="Name" onChange={handleNameChange} />
+          <hr style={{ marginTop: '2rem' }} />
+          <h2>Scores</h2>
+          <TextField error={errorField === 'maths'} label="Maths" value={mathsScore} onChange={(val) => handleScoreChange(val, "maths")} />
+          <TextField error={errorField === 'physics'} label="Physics" value={physicsScore} onChange={(val) => handleScoreChange(val, "physics")} />
+          <TextField error={errorField === 'chem'} label="Chemistry" value={chemScore} onChange={(val) => handleScoreChange(val, "chem")} />
           <Button variant="contained" color="primary" type="submit">Submit</Button>
         </form>
         <div><h3>Total: {total}</h3></div>
-        <div><h3>Percentage: {percentage}%</h3></div>
+        <div><h3>Percentage: {percentage?.toPrecision(4)}%</h3></div>
       </Container>
     </div>
   )
